7f9bcb2970b56d6511b11eb21b2c4c9f07d7cdbc
Jakub Korcz <jakub.korcz@lstsoft.com.pl>
2025-10-22 16:29:07 +0200

    Przerzucenie miejsc usingów

--- PhotoApp.Client/PhotoApp.Client/Connection/ApiConnection.cs
+++ PhotoApp.Client/PhotoApp.Client/Connection/ApiConnection.cs
@@ -1,13 +1,40 @@
-﻿using System.Net.Http;
+﻿using PhotoApp.Common.Models;
+using System.Text.Json;
 
 namespace PhotoApp.Client.Connection
 {
     public partial class ApiConnection : IDisposable
     {
         private readonly HttpClient _httpClient;
-        public ApiConnection(HttpClient httpClient) {
+        public ApiConnection(HttpClient httpClient)
+        {
             _httpClient = httpClient;
         }
         public void Dispose() { }
+
+        private async Task<ServerAuthResponse> SendPostRequestWithoutData(string url)
+        {
+            var response = await _httpClient.PostAsync(url, null);
+            var json = await response.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var obj = JsonSerializer.Deserialize<ServerAuthResponse>(json, options);
+            if (obj != null)
+            {
+                return obj;
+            }
+            else
+            {
+                return new ServerAuthResponse()
+                {
+                    Message = "Odczytany obiekt jest pusty",
+                    Success = false
+                };
+            }
+        }
     }
 }
--- PhotoApp.Client/PhotoApp.Client/Connection/ApiConnection_Users.cs
+++ PhotoApp.Client/PhotoApp.Client/Connection/ApiConnection_Users.cs
@@ -1,49 +1,28 @@
 ﻿using PhotoApp.Common.Models;
-using System;
-using System.Text.Json;
 
 namespace PhotoApp.Client.Connection
 {
     public partial class ApiConnection
     {
-        public async Task<ServerAuthResponse> Login(string username) {
+        public async Task<ServerAuthResponse> Login(string username)
+        {
             var url = $"https://localhost:7003/users/login/{username}";
             return await SendPostRequestWithoutData(url);
         }
-        public async Task<ServerAuthResponse> LoginVerify(string username, string code) {
+        public async Task<ServerAuthResponse> LoginVerify(string username, string code)
+        {
             var url = $"https://localhost:7003/users/login/{username}/{code}";
             return await SendPostRequestWithoutData(url);
         }
-        public async Task<ServerAuthResponse> Register(string username) {
+        public async Task<ServerAuthResponse> Register(string username)
+        {
             var url = $"https://localhost:7003/users/register/{username}";
             return await SendPostRequestWithoutData(url);
         }
-        public async Task<ServerAuthResponse> RegisterVerify(string username, string code) {
+        public async Task<ServerAuthResponse> RegisterVerify(string username, string code)
+        {
             var url = $"https://localhost:7003/users/register/{username}/{code}";
             return await SendPostRequestWithoutData(url);
-        } 
-
-        private async Task<ServerAuthResponse> SendPostRequestWithoutData(string url)
-        {
-            var response = await _httpClient.PostAsync(url, null);
-            var json = await response.Content.ReadAsStringAsync();
-
-            var options = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true
-            };
-
-            var obj = JsonSerializer.Deserialize<ServerAuthResponse>(json, options);
-            if (obj != null)
-            {
-                return obj;
-            }
-            else { 
-                return new ServerAuthResponse() { 
-                    Message = "Odczytany obiekt jest pusty",
-                    Success = false
-                };
-            }
         }
     }
 }
