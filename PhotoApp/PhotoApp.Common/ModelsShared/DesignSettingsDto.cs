a17eff8b72a88b74ed2f6eaa1fa7073a9f3e3a18
JakubKorcz <korczjakub@wp.pl>
2026-03-24 14:37:27 +0100

    Poprawka błedu z res.Success

--- PhotoApp/PhotoApp.Common/ModelsShared/DesignSettingsDto.cs
+++ PhotoApp/PhotoApp.Common/ModelsShared/DesignSettingsDto.cs
@@ -1,9 +1,4 @@
 ﻿using PhotoApp.Common.EnumShared;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhotoApp.Common.ModelsShared
 {
